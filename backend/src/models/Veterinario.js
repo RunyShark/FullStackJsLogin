94dc0034b3b5e143a78a938db8c861df8fb6e717
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-05-23 16:21:47 -0500

    hash password

--- backend/src/models/Veterinario.js
+++ backend/src/models/Veterinario.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const { generarId } = require("../helpers/generarId.js");
+
 const veterinarioSchema = mongoose.Schema({
   nombre: {
     type: String,
@@ -33,6 +35,18 @@ const veterinarioSchema = mongoose.Schema({
     type: Boolean,
     default: false,
   },
+});
+
+veterinarioSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) {
+      next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  } catch (error) {
+    console.log(`Hash ${error}`);
+  }
 });
 
 const Veterinario = mongoose.model("Veterinario", veterinarioSchema);
