c4ad46766f46c37259515e46b1ab5218b2ccc264
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-06-04 21:28:49 -0500

    Fecha

--- frontend/src/components/modules/Admin/Cards/Cards.jsx
+++ frontend/src/components/modules/Admin/Cards/Cards.jsx
@@ -1,15 +1,38 @@
 const Cards = ({ e }) => {
   const { nombre, propietario, email, fecha, sintomas } = e;
+
+  const formaterFecha = (fecha) => {
+    const nuevaFecha = new Date(fecha);
+    return new Intl.DateTimeFormat("es-MX", { dateStyle: "long" }).format(
+      nuevaFecha
+    );
+  };
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-md">
-      <p className="font-bold uppercase text-gray-500">
+      <p className="font-bold uppercase text-indigo-700 my-2">
         Nombre:{" "}
         <span className="font-normal normal-case text-black">{nombre}</span>
       </p>
-      <p>{propietario}</p>
-      <p>{email}</p>
-      <p>{fecha}</p>
-      <p>{sintomas}</p>
+      <p className="font-bold uppercase text-indigo-700 my-2">
+        Paciente:{" "}
+        <span className="font-normal normal-case text-black">
+          {propietario}
+        </span>
+      </p>
+      <p className="font-bold uppercase text-indigo-700 my-2">
+        Email de contacto:{" "}
+        <span className="font-normal normal-case text-black">{email}</span>
+      </p>
+      <p className="font-bold uppercase text-indigo-700 my-2">
+        Fecha de alta:{" "}
+        <span className="font-normal normal-case text-black">
+          {formaterFecha(fecha)}
+        </span>
+      </p>
+      <p className="font-bold uppercase text-indigo-700 my-2">
+        Sintomas:{" "}
+        <span className="font-normal normal-case text-black">{sintomas}</span>
+      </p>
     </div>
   );
 };
