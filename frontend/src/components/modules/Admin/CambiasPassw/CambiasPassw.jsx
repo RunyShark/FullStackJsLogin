17b6a43fa30e3f1155c228d9b859dce200672f90
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-06-05 20:44:35 -0500

    actu

--- frontend/src/components/modules/Admin/CambiasPassw/CambiasPassw.jsx
+++ frontend/src/components/modules/Admin/CambiasPassw/CambiasPassw.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import AdminNav from "../AdminNav/AdminNav";
 import Alerta from "../../../helpers/Alerta";
